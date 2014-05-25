f30781563874a4baaa6768afe38ede0214d89be4
Earlz <earlz@lastyearswishes.com>
2014-05-24 23:42:30 -0400

    Update client name and version number

--- src/version.cpp
+++ src/version.cpp
@@ -10,7 +10,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both fractalcoind and fractalcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Shibetoshi");
+const std::string CLIENT_NAME("fractal-reference");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   ""
