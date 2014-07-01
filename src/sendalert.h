f5537751bf09b7f46fdae233e1488fa2f14e62f9
Earlz <earlz@lastyearswishes.com>
2014-06-30 23:40:39 -0400

    add copyright from grumpycoin

--- src/sendalert.h
+++ src/sendalert.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2013-2079 Dr. Kimoto Chan
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// upstream from https://github.com/grumpycoin/grumpycoin-v.1.2
+
 #ifndef SENDALERT_H
 #define SENDALERT_H
 
